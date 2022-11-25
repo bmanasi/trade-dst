c2ff2b2a15dba2f79cc0c4a5c3e5e12a38f85a53
Manasi <40319992+bmanasi@users.noreply.github.com>
2022-11-25 23:17:40 +0530

    Update EWC_train.py

--- EWC_train.py
+++ EWC_train.py
@@ -86,7 +86,8 @@ model = globals()[args["decoder"]](
 avg_best, cnt, acc = 0.0, 0, 0.0
 weights_best = deepcopy(model.state_dict())
 try:
-    for epoch in range(100):
+#     for epoch in range(100):
+    for epoch in range(10):
         print("Epoch:{}".format(epoch))  
         # Run the train function
         pbar = tqdm(enumerate(train_single),total=len(train_single))
